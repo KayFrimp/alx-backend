9540e7e0d27b489746ff1f8a03562a1db4dc03c2
Kwabena A. Frimpong <kafrimpong23@gmail.com>
2024-02-07 03:00:49 +0000

    Parametrized template

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""Get locale from request"""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config(object):
+    """Contains supported Languages"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+app.config.from_object('3-app.Config')
+
+
+@babel.localeselector
+def get_locale():
+    """Determines the best match for supported Languages"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def home():
+    """Renders Home Page"""
+    return render_template("3-index.html")
+
+
+if __name__ == "__main__":
+    app.run("0.0.0.0", 5000)
