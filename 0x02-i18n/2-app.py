23dcd121ec0f35895cd0eeb13c7a9af58753a148
Kwabena A. Frimpong <kafrimpong23@gmail.com>
2024-02-07 02:15:40 +0000

    Get locale from request

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Basic Bebel setup"""
+"""Get locale from request"""
 
 from flask import Flask, render_template, request
 from flask_babel import Babel
@@ -13,7 +13,7 @@ class Config:
 
 
 app = Flask(__name__)
-app.config.from_object('1-app.Config')
+app.config.from_object('2-app.Config')
 babel = Babel(app)
 
 
@@ -26,7 +26,7 @@ def get_locale():
 @app.route('/')
 def home():
     """Renders Home Page"""
-    return render_template("1-index.html")
+    return render_template("2-index.html")
 
 
 if __name__ == "__main__":
