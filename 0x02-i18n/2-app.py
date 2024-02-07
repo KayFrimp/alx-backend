57cd105f013b1544bcc7113c98919004f601436c
Kwabena A. Frimpong <kafrimpong23@gmail.com>
2024-02-07 02:30:30 +0000

    Get locale from request

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -5,28 +5,30 @@ from flask import Flask, render_template, request
 from flask_babel import Babel
 
 
-class Config:
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config(object):
     """Contains supported Languages"""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
-app = Flask(__name__)
 app.config.from_object('2-app.Config')
-babel = Babel(app)
+
+
+@app.route('/')
+def home():
+    """Renders Home Page"""
+    return render_template("2-index.html")
 
 
 @babel.locale_selector
 def get_locale():
     """Determines the best match for supported Languages"""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
-
-
-@app.route('/', methods=['GET'], strict_slashes=False)
-def home():
-    """Renders Home Page"""
-    return render_template("2-index.html")
 
 
 if __name__ == "__main__":
