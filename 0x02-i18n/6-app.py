48f7e6c3c77988204ee3d970ee04e45e6b5eb985
Kwabena A. Frimpong <kafrimpong23@gmail.com>
2024-02-07 16:41:01 +0000

    Use user locale

---
+++ 0x02-i18n/6-app.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+"""Get locale from request"""
+
+from typing import Union
+from flask import Flask, g, render_template, request
+from flask_babel import Babel
+
+
+class Config(object):
+    """Contains supported Languages"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+app.config.from_object('6-app.Config')
+
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+@babel.localeselector
+def get_locale():
+    """Determines the best match for supported Languages"""
+    if request.args.get('locale'):
+        locale = request.args.get('locale')
+        if locale in app.config['LANGUAGES']:
+            return locale
+    elif g.user and g.user.get('locale') and g.user.get('locale')\
+            in app.config['LANGUAGES']:
+        return g.user.get('locale')
+    else:
+        return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+def get_user() -> Union[dict, None]:
+    """ Returns user dict if ID can be found """
+    if request.args.get('login_as'):
+        user = int(request.args.get('login_as'))
+        if user in users:
+            return users.get(user)
+    else:
+        return None
+
+
+@app.before_request
+def before_request():
+    """ Finds user and sets as global on flask.g.user """
+    g.user = get_user()
+
+
+@app.route('/')
+def home():
+    """Renders Home Page"""
+    return render_template("6-index.html")
+
+
+if __name__ == "__main__":
+    app.run("0.0.0.0", 5000)
