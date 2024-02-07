d442404d9e92ff921558df880c3b6860814db729
Kwabena A. Frimpong <kafrimpong23@gmail.com>
2024-02-07 01:57:21 +0000

    Basic Babel setup

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""Basic Bebel setup"""
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config:
+    """Contains supported Languages"""
+    LANGUAGES = ["en", "fr"]
+
+
+app = Flask(__name__)
+app.config.update({
+    'BABEL_DEFAULT_LOCALE': 'en',
+    'BABEL_DEFAULT_TIMEZONE': 'UTC'
+})
+app.config.from_object('1-app.Config')
+babel = Babel(app)
+
+
+@app.route('/')
+def home():
+    """Renders Home Page"""
+    return render_template("1-index.html")
+
+
+if __name__ == "__main__":
+    app.run("0.0.0.0", 5000)
